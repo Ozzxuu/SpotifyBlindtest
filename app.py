22b1178c61dba7cbc750a2f81a1f638e0b00be19
Ozzxuu <170020375+Ozzxuu@users.noreply.github.com>
2025-06-18 00:36:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -144,6 +144,8 @@ def play():
 @app.route("/history")
 def history():
     return jsonify(played_tracks_data)
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
