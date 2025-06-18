07a2afd998a4fb32c27c25b77940f6d01d39d871
Ozzxuu <170020375+Ozzxuu@users.noreply.github.com>
2025-06-18 08:14:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -147,5 +147,5 @@ def history():
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
 
